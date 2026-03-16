215657f99e294daf0a03a7216631800918f50edb
Mr X <93528905+JulesZYTB@users.noreply.github.com>
2026-03-16 10:19:08 +0100

    Init page and components crée router !

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,52 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './components/layout/layout';
+import Errors from './components/error/errors';
 
-createRoot(document.getElementById('root')!).render(
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <Errors />,
+    /* children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "signup",
+        element: <Form />,
+      },
+      {
+        path: "products/:id",
+        element: <Product />,
+      },
+      {
+        path: "users",
+        element: <Outlet />,
+        children: [
+          {
+            index: true,
+            element: <User />,
+            loader: usersLoader,
+          },
+          {
+            path: ":id",
+            element: <UserDetails />,
+          },
+        ],
+      },
+    ], */
+  },
+]);
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
