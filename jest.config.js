ed9a58d5e07b13e7ea03a424553f254016d40b63
Mr X <93528905+JulesZYTB@users.noreply.github.com>
2026-03-25 10:55:16 +0100

    Ajoute de quelque import dans client pour la bdd et ajoute de la jest config pour mieux fonctionnée et correction des different test.

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  projects: [
+    "<rootDir>/server/jest.config.js",
+    // "<rootDir>/client/jest.config.js", // If client has jest
+  ],
+};
