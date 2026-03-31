347312b5985c91e3293f9212ed3f9265bcabf8ff
Mr X <93528905+JulesZYTB@users.noreply.github.com>
2026-03-31 16:32:40 +0200

    Installation des packages nessaisaire pour les tests unitaire du fontend et creation de setup.ts pour clean lmes test a chaque run puis modification du vite config pour etre compatible avec le systeme actuelle.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,5 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +13,10 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
   },
 });
