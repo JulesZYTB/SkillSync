ad9a7a43f6ec2ebe76b3d5dd8f99226112d30d62
Mr X <93528905+JulesZYTB@users.noreply.github.com>
2026-04-27 01:41:35 +0200

    ajout docker et config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,9 +7,8 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      // methode secret hahaah pour cachée l'api on utilisée un proxy qui vas rediriger les requete "/api" vers l'api real sans que l'utilisateurs ou les personne mal intentionner puisse voir l'api real
       "/api": {
-        target: "http://localhost:3310",
+        target: process.env.VITE_API_URL || "http://localhost:3310",
         changeOrigin: true,
       },
     },
